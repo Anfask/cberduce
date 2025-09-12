b2c14921b01b746b8088a509130a286601a2dce8
Anfas K <100411700+Anfask@users.noreply.github.com>
2025-09-12 21:22:59 +0530

    Update route.ts

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app"
 import { getFirestore, collection, addDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2-4jj1LnzVQsiyiEpdbtITWrPWHp69HM",
-  authDomain: "cyberduce-fddd5.firebaseapp.com",
-  projectId: "cyberduce-fddd5",
-  storageBucket: "cyberduce-fddd5.firebasestorage.app",
-  messagingSenderId: "757636976825",
-  appId: "1:757636976825:web:da8c9bee037d4821380b68",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
